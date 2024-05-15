b724804ccf2f4eb135567ca1037db2c831044aa8
Emiliano <emikinderk@gmail.com>
2024-05-15 20:20:18 +0200

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,7 @@
-export function middleware(request) {
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+    console.log(request);
     try {
         if (request.geo?.latitude && request.geo?.longitude) {
             const res = NextResponse.next();
