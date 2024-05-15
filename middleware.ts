b438f64af401ffd4e15354ce1dba8433ceb4a891
Emiliano <emikinderk@gmail.com>
2024-05-15 20:23:41 +0200

    remove try catch and log

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,10 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-    console.log(request);
-    try {
-        if (request.geo?.latitude && request.geo?.longitude) {
-            const res = NextResponse.next();
-            res.headers.set("x-latitude", request.geo.latitude);
-            res.headers.set("x-longitude", request.geo.longitude);
-            return res;
-        }
-    } catch (e) {
-        console.error(e);
+    if (request.geo?.latitude && request.geo?.longitude) {
+        const res = NextResponse.next();
+        res.headers.set("x-latitude", request.geo.latitude);
+        res.headers.set("x-longitude", request.geo.longitude);
+        return res;
     }
 }
