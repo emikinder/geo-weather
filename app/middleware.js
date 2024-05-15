ff149431ef36bf47a78c5aaeee5e4f4bc7a7b779
Emiliano <emikinderk@gmail.com>
2024-05-15 19:45:14 +0200

    Add middleware to set latitude and longitude headers

---
+++ app/middleware.js
@@ -0,0 +1,8 @@
+export function middleware(req) {
+    if (req.geo?.latiude && req.geo?.longitude) {
+        const res = NextResponse.next();
+        res.headers.set("x-latitude", req.geo.latitude);
+        res.headers.set("x-longitude", req.geo.longitude);
+        return res;
+    }
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ["cdn.weatherapi.com"],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "cdn.weatherapi.com",
+                port: "",
+                pathname: "/weather/64x64/",
+            },
+        ],
+    },
+};
 
 export default nextConfig;
