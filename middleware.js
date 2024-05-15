3572a2257843b405fe383112aa9c5df7bf007fba
Emiliano <emikinderk@gmail.com>
2024-05-15 20:07:37 +0200

    handle error middleware

--- middleware.js
+++ middleware.js
@@ -1,8 +1,12 @@
-export function middleware(req) {
-    if (req.geo?.latitude && req.geo?.longitude) {
-        const res = NextResponse.next();
-        res.headers.set("x-latitude", req.geo.latitude);
-        res.headers.set("x-longitude", req.geo.longitude);
-        return res;
+export function middleware(request) {
+    try {
+        if (request.geo?.latitude && request.geo?.longitude) {
+            const res = NextResponse.next();
+            res.headers.set("x-latitude", request.geo.latitude);
+            res.headers.set("x-longitude", request.geo.longitude);
+            return res;
+        }
+    } catch (e) {
+        console.error(e);
     }
 }
